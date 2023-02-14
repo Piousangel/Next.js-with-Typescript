69f1e2302dc3798aa87aace694addc66f59cbad5
양현석 <yyh7654@gmail.com>
2023-02-15 03:03:07 +0900

    :sparkles: Main 화면 및 구글 로그인 버튼 컴포넌트 구현

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { NextPage } from "next";
-import { ServiceLayout } from "components/service_layout";
-
-const IndexPage: NextPage = () => {
-    return <ServiceLayout title="test">test</ServiceLayout>;
-};
-
-export default IndexPage;
